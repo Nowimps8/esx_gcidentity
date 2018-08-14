0c9646030317fb7b5ca03dab2c4c50ca03e028a2
Nowimps8 <31818901+Nowimps8@users.noreply.github.com>
2018-08-14 02:30:22 -1000

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,52 +1,46 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+
 function getIdentity(source, callback)
-  local identifier = GetPlayerIdentifiers(source)[1]
-  MySQL.Async.fetchAll("SELECT * FROM `users` WHERE `identifier` = @identifier", {['@identifier'] = identifier},
-  function(result)
-    if result[1]['firstname'] ~= nil then
-      local data = {
-        identifier    = result[1]['identifier'],
-        firstname     = result[1]['firstname'],
-        lastname      = result[1]['lastname'],
-        dateofbirth   = result[1]['dateofbirth'],
-        sex           = result[1]['sex'],
-        height        = result[1]['height'],
-        job        = result[1]['job'],
-      }
-      callback(data)
-    else
-      local data = {
-        identifier    = '',
-        firstname     = '',
-        lastname      = '',
-        dateofbirth   = '',
-        sex           = '',
-        height        = ''
-      }
+  local _source = source
+  local xPlayer = ESX.GetPlayerFromId(_source)
+  local identifier = GetPlayerIdentifiers(_source)[1]
+  
+  local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {
+      ['@identifier'] = identifier
+    })
+	  local user      	  = result[1]
+    local firstname     = user['firstname']
+    local lastname      = user['lastname']
+    local sex           = user['sex']
+    local dob           = user['dateofbirth']
+    local height        = user['height']
+	  local job			      = user['job']
+	
+	local data = {
+      job         = job,
+      firstname   = firstname,
+      lastname    = lastname,
+      sex         = sex,
+      dob         = dob,
+      height      = height
+    }
       callback(data)
     end
-  end)
-end
-
-
-
 
 RegisterServerEvent('gc:openIdentity')
 AddEventHandler('gc:openIdentity',function(other)
     getIdentity(source, function(data)
-        local gender
-        if data.sex == "male" then
-            gender = "h"
-        elseif data.g == "female" then
-            gender = "f"
-        end 
         TriggerClientEvent('gc:showItentity', tonumber(other), {
-            nom = data.lastname,
-            prenom = data.firstname,
-            dateNaissance = tostring(data.dateofbirth),
-            sexe = gender,
-            jobs = data.job,
-            taille = data.height,
-            id = identifier
+            nom             = data.lastname,
+            prenom          = data.firstname,
+            dateNaissance   = data.dob,
+            sexe            = data.sex,
+            jobs            = data.job,
+            taille          = data.height,
+            id              = data.identifier
         })
     end)
 end)
@@ -54,20 +48,14 @@ end)
 RegisterServerEvent('gc:openMeIdentity')
 AddEventHandler('gc:openMeIdentity',function()
     getIdentity(source, function(data)
-        local gender
-        if data.sex == "male" then
-            gender = "h"
-        elseif data.g == "female" then
-            gender = "f"
-        end
         TriggerClientEvent('gc:showItentity', source, {
-            nom = data.lastname,
-            prenom = data.firstname,
-            dateNaissance = tostring(data.dateofbirth),
-            sexe = gender,
-            jobs = data.job,
-            taille = data.height,
-            id = identifier
+            nom             = data.lastname,
+            prenom          = data.firstname,
+            dateNaissance   = data.dob,
+            sexe            = data.sex,
+            jobs            = data.job,
+            taille          = data.height,
+            id              = data.identifier
         })
     end)
 end)
